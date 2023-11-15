f94956e731d815f7d66e6a5890a730e4d1ce9651
Ronny Schlidt <r.schlidt1@web.de>
2023-11-15 09:58:40 +0100

    update wtest comments and script

--- wtest.c
+++ wtest.c
@@ -3,17 +3,29 @@
 #include <sys/time.h>
 #include "wapi.h"
 #include "wext.h"
+#include <windows.h>
+#include <stdio.h>
+#include "../wapi/wapi.h"
+#include "../wapi/wext.h"
+
+ 
 
 int main() {
+     if (wOpen("192.168.1.129") != WSUCCESS) {
+          printf("Error wOpen\n");
+          return -1;
+     }
 
-    wOpen("192.169.23.23");
-    
-    float ch2bus2lvl = 0.5;
-    wSetTokenFloat(CH_2_SEND_2_LVL, ch2bus2lvl);
+     float ch = 0.5;
+     wSetTokenFloat(CH_2_SEND_2_LVL, ch);
+     Sleep(1);
+     wClose();
+     return 0;
 
-    wClose();
-    return 0;
 }
 
 // gcc -O3 -Wall -I:C:\Users\rschl\OneDrive\Dokumente\GitHub\wapi -c -o wtest.o "wtest.c" 
+// gcc  -LC:\Users\rschl\OneDrive\Dokumente\GitHub\wapi\Windows -o wtest.exe wtest.o -lwapi -lws2_32 
 // gcc "-LC:C:\Users\rschl\OneDrive\Dokumente\GitHub\wapi\Windows" -o wtest.exe wtest.o -lwapi -lws2_32
+
+// gcc  -o wtest.exe wtest.o C:\Users\Ronny\Documents\GitHub\wapi\Windows\libwapi.a -lws2_32
